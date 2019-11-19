5692e8c75da6a74d314e1138208b1c453c12465c
samybakk <samy.bakkouche@student.umons.ac.be>
2019-11-19 15:55:33 +0100

    third

--- main.py
+++ main.py
@@ -23,33 +23,26 @@ def framing(signal,shifting_step=16000,frames_size = 16000) :
 
 
     sig_size = len(signal)
-    frames = np.empty()
+    frames = []
     i=0
     while True :
         if(i+frames_size <= sig_size):
             fr_act_size = i+frames_size
         else:
             fr_act_size = sig_size
-        frames+=signal[i:fr_act_size]
+        frames.append(signal[i:fr_act_size])
         i+=shifting_step
         if(i>sig_size):
             break
+    frames = np.array((frames))
+    return frames
 
-
-'''    nbr_frames = sig_time/step
-    frames = np.array()
-    for i in range(0,round(nbr_frames)) :
-        if i != nbr_frames :
-            frame = signal[Fs*step*i:Fs*step*(i+1)-1]
-
-        else :
-            frame = 0
-        frames += frame'''
 if __name__ == '__main__':
     Fs = 16000
-    x = np.linspace(0,99,1000)
+    x = np.linspace(0,99999,100000)
     signal = 2*np.sin(x)
     signal = norm(signal)
+    frames= framing(signal)
 
     plt.plot(signal)
     plt.show()
